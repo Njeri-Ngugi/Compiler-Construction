cb4fb506cf1572b7f187f20cb5c6fd4e2953094c
lkendi <lkendi003@gmail.com>
2024-05-15 06:25:02 +0300

    Write scanner tokens to file

---
+++ Scanner_Program/mini2.c
@@ -0,0 +1,7 @@
+
+int main() {
+    int x = 10, y = 5;
+    printf("Sum: %d\n", x + y);
+    printf("Difference: %d\n", x - y);
+    return 0;
+}
--- Scanner_Program/scanner.py
+++ Scanner_Program/scanner.py
@@ -1,3 +1,4 @@
+import os
 import re
 import time
 
@@ -15,11 +16,21 @@ token_types = {
     "operator": r'[+\-/%<>^=!~]|<<|>>|\+\+|\-\-|&&|\|\||\+=|-=|\=|/=|%=|<<=|>>=|&=|\|=|\^=|==|!=|<=|>=|->'
 }
 
-# Combined regex for all token types
 token_regex = '|'.join('(?P<{}>{})'.format(token_name, regex) for token_name, regex in token_types.items())
 
-# Function to tokenize a line of code and print tokens
-def tokenize_and_print_line(line, line_number):
+
+def tokenize_and_print_line(line, line_number, output_file):
+    """
+    Tokenizes a line of code and prints token types for the lexemes,
+    then writes the list of tokens and lexemes to a file.
+
+    Args:
+        line - line of code to tokenize
+        line_number - current line number in the input code
+        output_file - file to write the token list and lexemes to after tokenization
+    """
+
+    token_list = []
     print("============ LINE", line_number, "============")
     invalid_tokens = []
 
@@ -27,6 +38,7 @@ def tokenize_and_print_line(line, line_number):
         for name, value in match.groupdict().items():
             if value:
                 print("{:<16}: {}".format(name.capitalize(), value))
+                token_list.append((name.capitalize(), value))
                 break
 
     all_tokens = re.findall(r'(?<!\S)\S+|\b\w+\b', line)
@@ -35,21 +47,48 @@ def tokenize_and_print_line(line, line_number):
     if invalid_tokens:
         print("{:<16}: {}".format("Invalid tokens", invalid_tokens))
 
+    with open(output_file, "a") as output_file:
+        output_file.write(f"Line {line_number}: {token_list}\n")
 
-# scan a file and tokenize its content line by line
+
+
 def scan_file(file_path):
+    """
+    Scans a file and tokenizes its content line by line
+
+    Args:
+        file_path - path to the file with code to tokenize
+    """
     with open(file_path, 'r') as file:
         line_number = 1
         for line in file:
-            tokenize_and_print_line(line.strip(), line_number)
+            tokenize_and_print_line(line.strip(), line_number, output_file)
             line_number += 1
 
-# Entry point
+
+def generate_output_filename(input_file):
+    """
+    Generates a unique output filename based on the input filename
+
+    Args:
+        input_file - path to the input file
+
+    Returns:
+        output_file - unique output filename
+    """
+    base_name, ext = os.path.splitext(input_file)
+    output_file = base_name + ".txt"
+    count = 1
+    while os.path.exists(output_file):
+        output_file = f"{base_name}{count}.txt"
+        count += 1
+    return output_file
+
 if __name__ == "__main__":
     start_time = time.time()
-    file_path = 'miniC.c'
-
-    scan_file(file_path)
+    input_file = 'mini2.c'
+    output_file = generate_output_filename(input_file)
+    scan_file(input_file)
 
     end_time = time.time()
     execution_time = end_time - start_time
