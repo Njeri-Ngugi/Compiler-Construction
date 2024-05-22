16c72cf714d9d2ea5daef1da32e9ef4c3bad388f
lkendi <lkendi003@gmail.com>
2024-05-22 10:36:12 +0300

    Edit scanner

--- Scanner_Program/scanner.py
+++ Scanner_Program/scanner.py
@@ -18,39 +18,38 @@ token_types = {
 
 token_regex = '|'.join('(?P<{}>{})'.format(token_name, regex) for token_name, regex in token_types.items())
 
-
-def tokenize_and_print_line(line, line_number, output_file):
+def tokenize_and_print_line(line, line_number):
     """
-    Tokenizes a line of code and prints token types for the lexemes,
-    then writes the list of tokens and lexemes to a file.
+    Tokenizes a line of code and prints token types for the lexemes
 
     Args:
         line - line of code to tokenize
         line_number - current line number in the input code
-        output_file - file to write the token list and lexemes to after tokenization
+
+    Returns:
+        token_list - list of tokens and lexemes
     """
-
     token_list = []
-    print("============ LINE", line_number, "============")
-    invalid_tokens = []
+    if line.strip():
+        print("============ LINE", line_number, "============")
+        invalid_tokens = []
 
-    for match in re.finditer(token_regex, line):
-        for name, value in match.groupdict().items():
-            if value:
-                print("{:<16}: {}".format(name.capitalize(), value))
-                token_list.append((name.capitalize(), value))
-                break
+        for match in re.finditer(token_regex, line):
+            for name, value in match.groupdict().items():
+                if value:
+                    print("{:<16}: {}".format(name.capitalize(), value))
+                    token_list.append((name.capitalize(), value))
+                    break
 
-    all_tokens = re.findall(r'(?<!\S)\S+|\b\w+\b', line)
-    invalid_tokens = [token for token in all_tokens if not any(re.match(regex, token) for regex in token_types.values())]
+        all_tokens = re.findall(r'(?<!\S)\S+|\b\w+\b', line)
+        invalid_tokens = [token for token in all_tokens if not any(re.match(regex, token) for regex in token_types.values())]
 
-    if invalid_tokens:
-        print("{:<16}: {}".format("Invalid tokens", invalid_tokens))
+        if invalid_tokens:
+            print("{:<16}: {}".format("Invalid tokens", invalid_tokens))
+    else:
+        print("No tokens in line", line_number)
 
-    with open(output_file, "a") as output_file:
-        output_file.write(f"Line {line_number}: {token_list}\n")
-
-
+    return token_list
 
 def scan_file(file_path):
     """
@@ -58,37 +57,43 @@ def scan_file(file_path):
 
     Args:
         file_path - path to the file with code to tokenize
+
+    Returns:
+        token_list - list of tokens and lexemes for each line of code
     """
+    token_list = []
     with open(file_path, 'r') as file:
         line_number = 1
         for line in file:
-            tokenize_and_print_line(line.strip(), line_number, output_file)
+            token_list.append(tokenize_and_print_line(line.strip(), line_number))
             line_number += 1
+    return token_list # list of tuples (token_type, token_value)
+
+# def generate_output_filename(input_file):
+#     """
+#     Generates a unique output filename based on the input filename
+
+#     Args:
+#         input_file - path to the input file
+
+#     Returns:
+#         output_file - unique output filename
+#     """
+#     base_name, ext = os.path.splitext(input_file)
+#     output_file = base_name + ".txt"
+#     count = 1
+#     while os.path.exists(output_file):
+#         output_file = f"{base_name}{count}.txt"
+#         count += 1
+#     return output_file
 
 
-def generate_output_filename(input_file):
-    """
-    Generates a unique output filename based on the input filename
-
-    Args:
-        input_file - path to the input file
-
-    Returns:
-        output_file - unique output filename
-    """
-    base_name, ext = os.path.splitext(input_file)
-    output_file = base_name + ".txt"
-    count = 1
-    while os.path.exists(output_file):
-        output_file = f"{base_name}{count}.txt"
-        count += 1
-    return output_file
 
 if __name__ == "__main__":
     start_time = time.time()
-    input_file = 'mini2.c'
-    output_file = generate_output_filename(input_file)
-    scan_file(input_file)
+    input_file = '../Scanner_Program/mini2.c'
+    # output_file = generate_output_filename(input_file)
+    tokens = scan_file(input_file)
 
     end_time = time.time()
     execution_time = end_time - start_time
