5e19f12b4a900c62058a1e11efcb04e7559c7878
lkendi <lkendi003@gmail.com>
2024-05-22 10:19:24 +0300

    Edit working.py

--- Parser/working.py
+++ Parser/working.py
@@ -1,9 +1,33 @@
 from tabulate import tabulate
+import sys
+import os
+
+# import scanner from Scanner_Program in parent dir
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
+sys.path.append(parent_dir)
+
+from Scanner_Program.scanner import scan_file
+
+# Your other code follows...
+
+
 
 grammar = {
-  'Program': [['Function']],
-  'Function': [['int', 'main()', '{', 'ReturnStatement', '}']],
-  'ReturnStatement': [['return', '0;']]
+    'Program': [['int', 'main', 'OpenParen', 'CloseParen', 'OpenBrace', 'Stmts', 'ReturnStmt' ,'CloseBrace']],
+    'Stmts': [['Declaration'], ['Assignment'], ['Arithmetic']],
+    'Declaration': [['Type', 'Identifier', ';']],
+    'Assignment': [['Type', 'Identifier', '=', 'Num', ';']],
+    'Type': [['int'], ['char'], ['float']],
+    'Identifier': [['x'], ['y']],
+    'ReturnStmt': [['return', '0', ';']],
+    'Arithmetic': [['Num', 'Op', 'Num', ';']],
+    'Num': [['0']],
+    'Op': [['+']],
+    'OpenParen': [['(']],
+    'CloseParen': [[')']],
+    'OpenBrace': [['{']],
+    'CloseBrace': [['}']],
 }
 
 def find_first(grammar, non_terminal, visited=None):
@@ -20,7 +44,6 @@ def find_first(grammar, non_terminal, visited=None):
             first_set |= find_first(grammar, production[0], visited)
     return first_set
 
-
 def find_follow(grammar, non_terminal, start_symbol, follow_set=None):
     if follow_set is None:
         follow_set = set()
@@ -35,25 +58,26 @@ def find_follow(grammar, non_terminal, start_symbol, follow_set=None):
                     follow_set.add(production[index + 1])
                 elif production[index + 1] in grammar:
                     first_of_next = find_first(grammar, production[index + 1]) - {''}
-                    if '' in find_first(grammar, production[index + 1]):
+                    if '' in first_of_next:
                         follow_set |= find_follow(grammar, symbol, start_symbol, follow_set)
                     follow_set |= first_of_next
     if non_terminal == start_symbol:
         follow_set.add('$')
     return follow_set
 
-
-def generate_parse_table(grammar, first_sets, follow_sets):
+def generate_parse_table(grammar, first_sets, follow_sets, filename):
     tables = {}
     for non_terminal, productions in grammar.items():
         for production in productions:
             first_of_production = find_first(grammar, production[0])
             for terminal in first_of_production:
-                if terminal != '':
+                if terminal != '#':
                     tables[non_terminal, terminal] = production
-            if '' in first_of_production:
+            if '#' in first_of_production:
                 for terminal in follow_sets[non_terminal]:
                     tables[non_terminal, terminal] = production
+
+    # Create table data
     headers = list(set(terminal for non_terminal, terminal in tables))
     headers.sort()
     headers.insert(0, " ")
@@ -63,67 +87,85 @@ def generate_parse_table(grammar, first_sets, follow_sets):
         for terminal in headers[1:]:
             row.append(tables.get((non_terminal, terminal), " "))
         table.append(row)
-    print(tabulate(table, headers, tablefmt="grid"))
+
+    # Save table to file
+    with open(filename, "w") as file:
+        file.write(tabulate(table, headers, tablefmt="grid"))
+
+    print(f"\nSuccess: Parse table saved to {filename}\n")
     return tables
 
-def ll1_algorithm(parse_tables, start_symbol, input_string):
+
+def ll1_algorithm(parse_table, start_symbol, token_list):
     stack = ["$"]
     stack.append(start_symbol)
-    input_string = input_string.split()
+    input_string = [token[1] for token in token_list]
     input_string.append("$")
     input_string.reverse()
 
     header = ["stack", "input", "action"]
-    parse_table = []
+    table = []
     while stack and input_string:
-        row = [stack, ' '.join(input_string)]
+        row = [stack, list(input_string)]
+
         if stack[-1] == input_string[-1] == "$":
             row.append("String accepted")
-            parse_table.append(row)
+            table.append(row)
             break
+
         if stack[-1] == input_string[-1]:
             stack.pop()
             input_string.pop()
             row.append("pop")
-            parse_table.append(row)
+            table.append(row)
         else:
-            production = parse_tables.get((stack[-1], input_string[-1]))
+            production = parse_table.get((stack[-1], input_string[-1]))
+            print("Production: ", production)
             if production is None:
-                row.append("String not accepted")
-                parse_table.append(row)
+                row.append(f"Syntax error at token {input_string[-1]}")
+                table.append(row)
                 break
             stack.pop()
-            if production != "":
+            if production != "#":
                 production = list(production)
                 production.reverse()
                 stack += production
             row.append(production)
-            parse_table.append(row)
+            table.append(row)
+
         if not stack:
             stack.append("$")
         if not input_string:
             input_string.append("$")
-    print(tabulate(parse_table, header, tablefmt="grid"))
+
+    print(tabulate(table, header, tablefmt="grid") + "\n")
     return
 
 
-def main():
-  start_symbol = list(grammar.keys())[0]
-  first_sets = {non_terminal: find_first(grammar, non_terminal) for non_terminal in grammar}
-  follow_sets = {non_terminal: find_follow(grammar, non_terminal, start_symbol) for non_terminal in grammar}
 
-
-  print("\nFIRST sets:")
-  for non_terminal, first_set in first_sets.items():
-    print(f"-FIRST({non_terminal}) = {first_set}")
-  print("\nFOLLOW sets:")
-  for non_terminal, follow_set in follow_sets.items():
-    print(f"-FOLLOW({non_terminal}) = {follow_set}")
-
-  print("\nParse Table:")
-  parse_table = generate_parse_table(grammar, first_sets, follow_sets)
-  input_string = "int main() { return 0; }"
-  ll1_algorithm(parse_table, start_symbol, input_string)
+def use_tokens():
+    input_file = 'Scanner_Program/mini2.c'
+    tokens = scan_file(input_file)
+    all_tokens = [token for sublist in tokens for token in sublist]
+    return all_tokens
 
 if __name__ == "__main__":
-  main()
+    tokens = use_tokens()
+
+    start_symbol = list(grammar.keys())[0]
+    first_sets = {non_terminal: find_first(grammar, non_terminal) for non_terminal in grammar}
+    follow_sets = {non_terminal: find_follow(grammar, non_terminal, start_symbol) for non_terminal in grammar}
+
+    print("\nFIRST sets:")
+    for non_terminal, first_set in first_sets.items():
+        print(f"-FIRST({non_terminal}) = {first_set}")
+
+    print("\nFOLLOW sets:")
+    for non_terminal, follow_set in follow_sets.items():
+        print(f"-FOLLOW({non_terminal}) = {follow_set}")
+
+    parse_table = generate_parse_table(grammar, first_sets, follow_sets, "parse_table.txt")
+
+
+    # parsing
+    ll1_algorithm(parse_table, start_symbol, tokens)
--- Scanner_Program/mini2.c
+++ Scanner_Program/mini2.c
@@ -1,7 +1,6 @@
 
-int main() {
-    int x = 10, y = 5;
-    printf("Sum: %d\n", x + y);
-    printf("Difference: %d\n", x - y);
-    return 0;
+int main () {
+
+   int x = 0 ;
+    return 0 ;
 }
